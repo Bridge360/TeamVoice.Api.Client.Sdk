4aadef0c8a17bfd66cf1a033000cfe19789b986b
Threetwosevensixseven <robin.guest@gmail.com>
2016-03-05 23:49:30 -0500

    Added nuget package metadata.

--- TeamVoice.Api.Client.Sdk/Current/Current.cs
+++ TeamVoice.Api.Client.Sdk/Current/Current.cs
@@ -8,8 +8,8 @@ namespace TeamVoice.Api
 {
     public abstract partial class Current
     {
-        public const string Product = "TeamVoice³";
-        public const string Description = "TeamVoice REST API";
+        public const string Product = "TeamVoice";
+        public const string Description = "The C# SDK and reference implementation of the TeamVoice REST API.";
         public const string Company = "Bridge³";
         public const string Copyright = "Copyright © 2007-" + Year + " Bridge³";
         public const string Version = "1.0.0.0";
